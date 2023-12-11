70afa0641e1d6fe8411f50defaf8da3429e7188c
daftmaple <ubuntumaple@gmail.com>
2023-12-11 19:54:15 +1100

    Vite + tailwind template

---
+++ src/App.tsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="max-w-[80rem] mx-auto my-0 text-center">
+      <div className="flex flex-nowrap justify-center">
+        <a href="https://vitejs.dev" target="_blank">
+          <img
+            src={viteLogo}
+            className="h-[6em] p-[1.5em] logo"
+            alt="Vite logo"
+          />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img
+            src={reactLogo}
+            className="h-[6em] p-[1.5em] logo react"
+            alt="React logo"
+          />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="p-[2em]">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="text-[#888888]">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  );
+}
+
+export default App;
