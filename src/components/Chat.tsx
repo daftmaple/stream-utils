b4625582edb2c03f4218ae461fa9e1811701a736
daftmaple <ubuntumaple@gmail.com>
2023-12-13 00:30:04 +1100

    Add emote renderer

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { ChatUserstate } from "tmi.js";
 
 interface ChatProps {
@@ -6,9 +7,42 @@ interface ChatProps {
 }
 
 export function Chat({ userstate, message }: ChatProps) {
+  const renderMessage = () => {
+    const finalString = message.split(" ");
+
+    const emotes = Object.fromEntries(
+      Object.entries(userstate.emotes ?? {}).map(([emote, replaceLocation]) => {
+        const [startRange, endRange] = replaceLocation[0].split("-");
+        const textToReplace = message.substring(
+          Number(startRange),
+          Number(endRange) + 1
+        );
+
+        return [textToReplace, emote];
+      })
+    );
+
+    console.log(emotes);
+
+    const finalMessage = finalString.map((text, index) => {
+      if (emotes[text]) {
+        const emoteUrl = `https://static-cdn.jtvnw.net/emoticons/v1/${emotes[text]}/1.0`;
+        return (
+          <>
+            <img className="inline" src={emoteUrl} alt={text} key={index} />{" "}
+          </>
+        );
+      }
+
+      return <React.Fragment key={index}>{text} </React.Fragment>;
+    });
+
+    return finalMessage;
+  };
+
   return (
     <div className="border-0 border-solid rounded border-teal-400 m-2">
-      <b>{userstate["display-name"]}</b>: {message}
+      <b>{userstate["display-name"]}</b>: {renderMessage()}
     </div>
   );
 }
