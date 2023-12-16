70d06c2a1967cff94e3e701ecdf3f580a1334a3a
daftmaple <ubuntumaple@gmail.com>
2023-12-17 05:55:03 +1100

    Add image skeleton style

--- src/pages/Lastfm.tsx
+++ src/pages/Lastfm.tsx
@@ -2,12 +2,17 @@ import { useQuery } from "@tanstack/react-query";
 import clsx from "clsx";
 import { useParams, useSearchParams } from "react-router-dom";
 import { LastFmResponse, getImage } from "../utils/lastfm";
+import { IconLoader, IconMoodSad, IconSkull } from "@tabler/icons-react";
 
 // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
 const LASTFM_API_KEY: string = import.meta.env.VITE_LASTFM_API_KEY ?? "";
 const LASTFM_API_URL = "https://ws.audioscrobbler.com/2.0";
 
-const baseWidgetStyle = "w-[32rem] absolute top-3 rounded bg-gray-500/75 p-3";
+const baseWidgetStyle =
+  "w-[32rem] absolute top-3 rounded bg-gray-500/75 p-3 flex flex-row gap-2";
+
+const skeletonImageStyle =
+  "bg-gray-200 flex justify-center items-center h-32 w-32";
 
 export function Lastfm() {
   const { username = "daftmaple" } = useParams();
@@ -45,7 +50,7 @@ export function Lastfm() {
     const imageUrl = getImage(lastTrack.image, "extralarge");
 
     return (
-      <div className={clsx(widgetStyle, "flex flex-row gap-2")}>
+      <div className={widgetStyle}>
         <img
           src={imageUrl["#text"]}
           alt={lastTrack.name}
@@ -63,14 +68,31 @@ export function Lastfm() {
   }
 
   return (
-    <div className={clsx(widgetStyle, "text-slate-100")}>
+    <div className={widgetStyle}>
       {error ? (
-        <>Failed to fetch from Lastfm</>
+        <>
+          <div className={skeletonImageStyle}>
+            <IconMoodSad className="text-black h-12 w-12" />
+          </div>
+          <p className="text-md mb-2 text-slate-100">
+            Failed to fetch from Lastfm
+          </p>
+        </>
       ) : isLoading ? (
-        <>Loading...</>
+        <>
+          <div className={skeletonImageStyle}>
+            <IconLoader className="text-black h-12 w-12" />
+          </div>
+          <p className="text-md mb-2 text-slate-100">Loading...</p>
+        </>
       ) : (
         <>
-          Missing <code>LASTFM_API_KEY</code>
+          <div className={skeletonImageStyle}>
+            <IconSkull className="text-black h-12 w-12" />
+          </div>
+          <p className="text-md mb-2 text-slate-100">
+            Missing <code>LASTFM_API_KEY</code>
+          </p>
         </>
       )}
     </div>
